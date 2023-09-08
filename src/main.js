2de7ae687cf56bbf59fbbdf9010faa6d1f8ea74d
xiangnan <280145668@qq.com>
2023-09-08 13:12:17 +0800

    eslint

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-var x = (a) => (1 ? 2 : 3);
+let a = 1,
+  b = 1,
+  c = 1;
+var x = (a) => (c ? 2 : 3);
 var foo = a + b * c;
-var foo = (a && b) || c;
 createApp(App).mount("#app");
+var addNumber = () => { };
 var hello = "world"[(1, 2, 3)].forEach(addNumber);
+var text = "";
+var position = 1;
 const value = text.trim().split("\n")[position].toLowerCase();
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/',
-  plugins: [eslintPlugin(), vue()]
-})
+  plugins: [vue()],
+});
