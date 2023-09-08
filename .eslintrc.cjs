651f011de5c25f863020ac727d50c2b247629b42
xiangnan <280145668@qq.com>
2023-09-08 17:52:27 +0800

    use-latest-vite4

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,51 +1,23 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "plugin:prettier/recommended"],
-  // plugins: ['prettier'], // seems place prettier at the last
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
   parserOptions: {
-    sourceType: "module",
-    ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
-  },
-  globals: {
-    $nuxt: true,
+    ecmaVersion: 'latest'
   },
   rules: {
-    "prettier/prettier": [
-      "warn",
-      {
-        // printWidth: 90,
-        semi: true,
-        trailingComma: "all",
-        arrowParens: "always",
-        bracketSpacing: true,
-        endOfLine: "lf",
-        htmlWhitespaceSensitivity: "css",
-        insertPragma: false,
-        singleAttributePerLine: false,
-        bracketSameLine: false,
-        // jsxBracketSameLine: false,
-        // jsxSingleQuote: false,
-        proseWrap: "preserve",
-        quoteProps: "as-needed",
-        requirePragma: false,
-        singleQuote: false,
-        tabWidth: 2,
-        useTabs: false,
-        embeddedLanguageFormatting: "auto",
-        vueIndentScriptAndStyle: false,
-      },
-    ],
-    "no-unused-vars": "warn",
+    'no-unused-vars': 'warn',
     // "max-len": ["error", { code: 94, ignoreUrls: true }],
-    "no-mixed-operators": "off",
-    "no-unexpected-multiline": "off",
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-  },
-};
+    'no-mixed-operators': 'off',
+    'no-unexpected-multiline': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
